19eea5ed344fcd000b57b327903de31c02de757b
XuJun <nankaimathxujun@gmail.com>
2017-06-09 10:11:13 +0800

    add

---
+++ LSRposi_MS/SubspaceSegmentation.m
@@ -0,0 +1,23 @@
+function missrate = SubspaceSegmentation( SegmentatiomMethod , X , gnd , Par )
+
+switch SegmentatiomMethod
+    
+    case 'LSR1'
+        C = LSR1( X , Par ) ;
+        
+    case 'LSR2'
+        C = LSR2( X , Par ) ;
+        
+    case 'LSRd0po'
+        C = LSRd0po( X , Par ) ;
+        
+    case 'LSRpo'
+        C = LSRpo( X , Par ) ;
+        
+end
+
+nCluster = length( unique( gnd ) ) ;
+Z = ( abs(C) + abs(C') ) / 2 ;
+idx = clu_ncut(Z,nCluster) ;
+acc = compacc(idx,gnd) ;
+missrate = 1 - acc;
