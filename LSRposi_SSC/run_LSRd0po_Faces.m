3b44a91340b7bacc14a6212b0f5e63b386205fa9
XuJun <nankaimathxujun@gmail.com>
2017-06-14 18:45:23 +0800

    add

--- LSRposi_SSC/run_LSRd0po_Faces.m
+++ LSRposi_SSC/run_LSRd0po_Faces.m
@@ -12,34 +12,37 @@ method = 'LSRd0po_SSC';
 Par.thr = 2*10^-4;
 for mu = [1]
     Par.mu = mu;
-    for maxIter = 150
+    for maxIter = [5 10 15 20]
         Par.maxIter = maxIter;
-        for rho = [20]
+        for rho = [1e-4 1e-3 1e-2 0.1 1]
             Par.rho = rho;
-            for nSet = 5 %[2 3 4 5 6 7 8 9 10]
-                for i = 1:length(nSet)
-                    n = nSet(i);
-                    idx = Ind{n};
-                    for j = 1:size(idx,1)
-                        X = [];
-                        for p = 1:n
-                            X = [X Y(:,:,idx(j,p))];
+            for lambda = [1e-4 1e-3 1e-2 0.1 1]
+                Par.lambda = lambda;
+                for nSet = 2 %[2 3 4 5 6 7 8 9 10]
+                    for i = 1:length(nSet)
+                        n = nSet(i);
+                        idx = Ind{n};
+                        for j = 1:size(idx,1)
+                            X = [];
+                            for p = 1:n
+                                X = [X Y(:,:,idx(j,p))];
+                            end
+                            [D,N] = size(X);
+                            
+                            r = 0; affine = false; outlier = true; rho = 1;
+                            time0  =   clock;
+                            missrate= LSRd0posi(X,r,affine,outlier,rho,s{n},Par);
+                            fprintf('missrate: %2.4f\n', missrate);
+                            missrateTot{n}(j) = missrate;
                         end
-                        [D,N] = size(X);
-                        
-                        r = 0; affine = false; outlier = true; rho = 1;
-                        time0  =   clock;
-                        missrate= LSRd0posi(X,r,affine,outlier,rho,s{n},Par);
-                        fprintf('missrate: %2.4f\n', missrate);
-                        missrateTot{n}(j) = missrate;
+                        avgmissrate(n) = mean(missrateTot{n});
+                        medmissrate(n) = median(missrateTot{n});
+                        matname = sprintf(['C:/Users/csjunxu/Desktop/SC/Results/YaleB_' method '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_mu' num2str(Par.mu) '.mat']);
+                        save(matname,'missrateTot','avgmissrate','medmissrate');
                     end
-                    avgmissrate(n) = mean(missrateTot{n});
-                    medmissrate(n) = median(missrateTot{n});
                     matname = sprintf(['C:/Users/csjunxu/Desktop/SC/Results/YaleB_' method '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_mu' num2str(Par.mu) '.mat']);
                     save(matname,'missrateTot','avgmissrate','medmissrate');
                 end
-                matname = sprintf(['C:/Users/csjunxu/Desktop/SC/Results/YaleB_' method '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_mu' num2str(Par.mu) '.mat']);
-                save(matname,'missrateTot','avgmissrate','medmissrate');
             end
         end
     end
