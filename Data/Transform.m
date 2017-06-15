e342283cc55efe9835eb7aa58b13fb1927024b64
XuJun <nankaimathxujun@gmail.com>
2017-06-15 14:50:01 +0800

    add

--- Data/Transform.m
+++ Data/Transform.m
@@ -1,15 +1,23 @@
 clear;
 
-load 'YaleB.mat';
+%% USPS
+load 'USPS.mat';
+fea = fea';
+
+
+% %% YaleB = 'YaleB.mat';
+% load 'YaleBa.mat';
+
+
 
 nSet = max(gnd);
 
 Y = cell(nSet, 1);
 S = cell(nSet, 1);
 for i = 1:nSet
-    Y{i, 1} = fea(:, find(gnd==i));
+    Y{i, 1} = fea(:, gnd==i);
     S{i, 1} = gnd(find(gnd==i));
 end
 
-save YaleB_Crop.mat Y S Ind;
+save USPSa.mat Y S USPSInd;
     
