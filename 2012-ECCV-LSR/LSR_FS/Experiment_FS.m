990ef4c14d780fecb7859fa57c1be3a524298205
XuJun <nankaimathxujun@gmail.com>
2017-06-15 10:47:16 +0800

    add

--- 2012-ECCV-LSR/LSR_FS/Experiment_FS.m
+++ 2012-ECCV-LSR/LSR_FS/Experiment_FS.m
@@ -18,11 +18,12 @@ fprintf('\n\n**************************************   %s   *********************
 fprintf( [ mfilename(currentpath) ' Begins.\n' ] ) ;
 fprintf( [ mfilename(currentpath) ' is going, please wait...\n' ] ) ;
 
-%% Data YaleB
-load 'C:\Users\csjunxu\Desktop\SC\2012-ECCV-LSR\LSR_FS\Data\YaleB.mat'              % load YaleB dataset
-
 
 for nCluster = [2 3 4];           % number of subspace, 5 or 10 used in our paper
+    %% Data YaleB
+    load 'C:\Users\csjunxu\Desktop\SC\2012-ECCV-LSR\LSR_FS\Data\YaleB.mat'              % load YaleB dataset
+    
+    
     num = nCluster * 64 ;    % number of data used for subspace segmentation
     fea = fea(:,1:num) ;
     gnd = gnd(:,1:num) ;
