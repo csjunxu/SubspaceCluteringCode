6b09c4e8663de4a113b5af3da67e986a23abc8c0
XuJun <nankaimathxujun@gmail.com>
2017-06-17 12:47:46 +0800

    add

--- 2012-ECCV-LSR/LSR_FS/Experiment_FS.m
+++ 2012-ECCV-LSR/LSR_FS/Experiment_FS.m
@@ -78,7 +78,7 @@ for lambda = [.05 .1 .2 .3]
         avgmissrate(n) = mean(missrateTot{n});
         medmissrate(n) = median(missrateTot{n});
         fprintf('Total mean missrate  is %.3f%%.\n ' , avgmissrate(n)) ;
-        matname = sprintf([writefilepath 'YaleB_Crop_' SegmentationMethod '_DR' num2str(dim) '_lambda' num2str(lambda) '.mat']);
+        matname = sprintf([writefilepath 'YaleB_Crop_' SegmentationMethod '_DR' num2str(dim) '_dim' num2str(dim) '_lambda' num2str(lambda) '.mat']);
         save(matname,'missrateTot','avgmissrate','medmissrate');
     end
 end
