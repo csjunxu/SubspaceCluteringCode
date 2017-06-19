95c05dc771a746d1719e8908226c1a1ea3aa650e
XuJun <nankaimathxujun@gmail.com>
2017-06-19 22:36:56 +0800

    add

--- LSRposi_MS/Demo_Digt_NNLSR.m
+++ LSRposi_MS/Demo_Digt_NNLSR.m
@@ -31,9 +31,9 @@ SegmentationMethod = 'ANNLSR' ;
 %% Subspace segmentation
 for maxIter = [5 10]
     Par.maxIter = maxIter;
-    for rho = [0.05 0.06 0.04 0.07 0.03]
+    for rho = [0.0001 0.0005 0.001 0.005 0.01 0.1 1 5]
         Par.rho = rho;
-        for lambda = [2:1:6]
+        for lambda = [1:1:6]
             Par.lambda = 10^(-lambda);
             for nSet = [2 3 5 8 10] % nSet = [2:1:10];
                 n = nSet;
