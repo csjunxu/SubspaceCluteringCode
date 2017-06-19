91f560a63f90554dcb2aefda852107a3618a8f0b
XuJun <nankaimathxujun@gmail.com>
2017-06-19 22:26:29 +0800

    add

--- SSCOMP_Code/Demo_EYaleB.m
+++ SSCOMP_Code/Demo_EYaleB.m
@@ -31,7 +31,7 @@
 % load 'C:\Users\csjunxu\Desktop\SC\Datasets\YaleB_Crop.mat'   % load YaleB dataset
 load 'C:\Users\csjunxu\Desktop\SC\Datasets\USPS_Crop.mat'   % load USPS dataset
 
-dataset = 'USPS';
+dataset = 'USPS'; % YaleB_LSR
 SegmentationMethod = 'SSC_OMP';
 writefilepath = 'C:/Users/csjunxu/Desktop/SC/Results/';
 % writefilepath = '';
