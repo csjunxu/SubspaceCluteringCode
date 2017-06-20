57a37737b104ac2cdc7353ce88058029ae52010a
XuJun <nankaimathxujun@gmail.com>
2017-06-20 09:33:54 +0800

    add

--- RenameFiles.m
+++ RenameFiles.m
@@ -1,6 +1,7 @@
 clear;
+
 Original_image_dir = 'C:\Users\csjunxu\Desktop\SC\Results\';
-fpath = fullfile(Original_image_dir, 'YaleB*.mat');
+fpath = fullfile(Original_image_dir, 'YaleB_ANNLSR_SSC_DR1*.mat');
 mat_dir  = dir(fpath);
 mat_num = length(mat_dir);
 
@@ -9,9 +10,8 @@ for i = 1 : mat_num
     fprintf([mat_dir(i).name '\n']);
     eval(['load C:\Users\csjunxu\Desktop\SC\Results\' mat_dir(i).name]);
     meanavgmissrate = [meanavgmissrate mean(avgmissrate)];
-    fprintf([num2str(meanavgmissrate) '\n']);
 end
 
 [minmeanavgmissrate, index] = sort(meanavgmissrate, 'ascend');
-
+fprintf([num2str(minmeanavgmissrate(1)) '\n']);
 fprintf('The minimum one is %s', mat_dir(index(1)).name);
