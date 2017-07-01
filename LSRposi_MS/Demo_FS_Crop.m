eed28d20849ab9339d38825ddf5500af3aceca7e
XuJun <nankaimathxujun@gmail.com>
2017-07-01 14:12:29 +0800

    add

--- LSRposi_MS/Demo_FS_Crop.m
+++ LSRposi_MS/Demo_FS_Crop.m
@@ -25,20 +25,20 @@ end
 % SegmentationMethod = 'NNLSR' ;
 % SegmentationMethod = 'NNLSRd0' £»
 % SegmentationMethod = 'NPLSR' ;
-SegmentationMethod = 'NPLSRd0' ;
+% SegmentationMethod = 'NPLSRd0' ;
 % find a fast solver is still in process
 
 % SegmentationMethod = 'ANNLSR' ;
 % SegmentationMethod = 'ANNLSRd0' ;
 % SegmentationMethod = 'ANPLSR' ;
-% SegmentationMethod = 'ANPLSRd0' ;
+SegmentationMethod = 'ANPLSRd0' ;
 
 %% Subspace segmentation
 for maxIter = [5]
     Par.maxIter = maxIter;
     for rho = [0.01:0.01:0.1]
         Par.rho = rho;
-        for lambda = [0 1]
+        for lambda = [0 1 10]
             Par.lambda = lambda*10^(-4);
             for nSet = [2 3 5 8 10]
                 n = nSet;
